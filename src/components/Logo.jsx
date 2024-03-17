106ee49518ebb3b419ea93a69b9290b42e0a8319
VishakhaSenthilnathan <vishakhasenthilnathan@gmail.com>
2024-03-18 00:23:34 +0530

    fix 3

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,4 @@
-import LogoImage from "../assets/logo.png"
-export default function Logo(){
-    return (
-        <img src={LogoImage} alt="Logo" width={67}/>
-    )
+import LogoImage from "../assets/logo.png";
+export default function Logo() {
+	return <img src={LogoImage} alt="Logo" width={67} height={34} />;
 }
