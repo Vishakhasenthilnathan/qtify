343508c0fcb54e008d0f1b8bea35daa133052f39
VishakhaSenthilnathan <vishakhasenthilnathan@gmail.com>
2024-03-18 00:08:57 +0530

    test fix - navbar 1

--- src/Theme.js
+++ src/Theme.js
@@ -1,53 +1,37 @@
-
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    spacing: 4,
-    typography: {
-        fontFamily: [
-            'Poppins',
-        ].join(','),
-        // h1: {
-        //     fontSize: '5rem',
-        //     fontFamily: 'Raleway',
-        // },
-        // h2: {
-        //     fontSize: '3.5rem',
-        //     fontFamily: 'Open Sans',
-        //     fontStyle: 'bold',
-        // },
-        // h3: {
-        //     fontSize: '2.5rem',
-        //     fontFamily: 'Roboto',
-        // },
-    },
-    palette: {
-        background: {
-            default: '#34C94B'//green
-        },
-        primary: {
-            main: '#121212',//indigo
-        },
-        secondary: {
-            main: '#FFFFFF',//pink
-        },
-        error: {
-            main: '#D72A2A',//red
-        },
-        warning: {
-            main: '#FC7B09',//orange
-        },
-        info: {
-            main: '#6B7D6A',//gray
-        },
-        success: {
-            main: '#09FE00',//green
-        },
-        text: {
-            primary: '#09FE00',//black
-            secondary: '#FFFFFF',//white
-        },
-    },
+	spacing: 4,
+	typography: {
+		fontFamily: ["Poppins"].join(","),
+	},
+	palette: {
+		background: {
+			default: "#34C94B", //green
+		},
+		primary: {
+			main: "#121212", //indigo
+		},
+		secondary: {
+			main: "#FFFFFF", //pink
+		},
+		error: {
+			main: "#D72A2A", //red
+		},
+		warning: {
+			main: "#FC7B09", //orange
+		},
+		info: {
+			main: "#6B7D6A", //gray
+		},
+		success: {
+			main: "#09FE00", //green
+		},
+		text: {
+			primary: "#09FE00", //black
+			secondary: "#FFFFFF", //white
+		},
+	},
 });
- 
+
 export default theme;
