4fb74ba9313baaf5af024e6cdddf2f21ebffcc78
VishakhaSenthilnathan <vishakhasenthilnathan@gmail.com>
2024-03-18 22:51:41 +0530

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import React, { Component } from "react";
 import "./App.css";
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Theme";
-import NavBar from "./components/NavBar/NavBar"
-
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
-  return (
-    <React.Fragment>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <NavBar/>
-        </div>
-      </ThemeProvider>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<ThemeProvider theme={theme}>
+				<div className="App">
+					<NavBar />
+					<Hero />
+				</div>
+			</ThemeProvider>
+		</React.Fragment>
+	);
 }
 
 export default App;
