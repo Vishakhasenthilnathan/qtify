979ce35d49c1dfad8aaa6e0526bc40fef1b158a2
VishakhaSenthilnathan <vishakhasenthilnathan@gmail.com>
2024-03-18 22:58:04 +0530

    Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
 	return (
 		<div className={styles.hero}>
 			<div style={{ lineHeight: "0.7", alignSelf: "center" }}>
-				<h1>100 Thousand songs, ad-free</h1>
+				<h1>100 Thousand Songs, ad-free</h1>
 				<h1>Over Thousand podcast episodes</h1>
 			</div>
 			<div>
